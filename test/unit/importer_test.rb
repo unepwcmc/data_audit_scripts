708212539ab7a8cd0acd710dfa391b16f87b4c56
Miguel Torres <migtorres@gmail.com>
2015-05-13 16:21:08 +0100

    adds importer test

---
+++ test/unit/importer_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+require 'csv'
+
+class TestImporter < ActiveSupport::TestCase
+
+  def setup
+    @filename = 'long_tables.csv'
+  end
+
+  test '.find_fields creates a hash with columns of model and values' do
+
+    FactoryGirl.create(:column_match, model_columns: 'pa_name_mett', access_columns: 'name in file')
+    FactoryGirl.create(:column_match, model_columns: 'research', access_columns: 'Research')
+
+    csv_values = { 'name in file' => 'wolf', 'Research' => 4 }
+
+    result = { pa_name_mett:  'wolf', research: 4 }
+
+    importer = Importer.new(filename: @filename)
+
+    assert_equal result, importer.find_fields(csv_values: csv_values)
+  end
+
+  test '.find_fields ignores columns not matching' do
+
+    FactoryGirl.create(:column_match, model_columns: 'pa_name_mett', access_columns: 'name in file')
+    FactoryGirl.create(:column_match, model_columns: 'research', access_columns: 'Research')
+
+    csv_values = { 'name in file' => 'wolf', 'name we dont want' => 'human', 'Research' => 4 }
+
+    result = { pa_name_mett:  'wolf', research: 4 }
+
+    importer = Importer.new(filename: @filename)
+
+    assert_equal result, importer.find_fields(csv_values: csv_values)
+  end
+
+end
